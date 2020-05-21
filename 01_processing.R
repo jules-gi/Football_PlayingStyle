9e92c8b45e13962c6a20da9938728ec65d046f65
Jules Girard <jules.girard@outlook.com>
2020-05-21 23:24:58 +0200

    Implement counter attack speed computation

--- 01_processing.R
+++ 01_processing.R
@@ -86,32 +86,101 @@ lapply(packages_list, library, character.only=TRUE)
   }
   
   
-  get_directPlay_coef = function(event_length, target_length, event_adjusted_angle, sigma=5) {
+  logistic_scale = function(value, sigma=10) {
     
-    ### Compute the relevance of the event comparing event length to target distance.
-    ### If event length is greater than target distance the coefficient decrease.
-    ### Coefficient is a measure of the ratio between extra distance covered by the event
-    ### and the target distance, then using logistic function to get coefficient :
-    ###     - if extra-distance is less than target distance (or backward direction), coeffcient is 1
-    ###     - if extra-distance is half target distance, coefficient is 0.5
-    ###     - if extra-distance is same as target distance, coefficient is 0
-    ### 
+    ### Convert values between 0 and 1 to logistic scale.
+    ###
     ### Inputs :
-    ###  - event_length (num), length of the event (pass / carry)
-    ###  - target_length (num), distance between player and target
-    ###  - event_adjusted_angle (num), adjusted angle between target direction and event direction
-    ###  - sigma (num, default=5), greater than 0, manage logistic slope (the lower the smoother)
+    ###   - value (num), values to convert
+    ###   - sigma (num, default=10), slope coefficient
     ###
     ### Output :
-    ###  - coefficient (num), between 0 (too long event) and 1 (less or equal to target distance)
+    ###   - logistic value conversion (num), between 0 and 1
     
-    if (sigma < 0) stop(paste0("'sigma' should be positive : sigma=", sigma))
+    if (sigma == 0) {
+      logistic_value = rep(0, length(value))
+    } else {
+      coef = .5 / abs(.5 - (1 / (1 + exp(-sigma * .5))))
+      tmp_logistic_value = (1 / (1 + exp(-sigma * (value - .5)))) - .5
+      logistic_value = tmp_logistic_value * coef + .5
+      
+      logistic_value[value < 0] = ifelse(sign(sigma) > 0, 0, 1)
+      logistic_value[value > 1] = ifelse(sign(sigma) > 0, 1, 0)
+    }
     
-    coef = ifelse((event_length > target_length) & cos(event_adjusted_angle) > 0,
-                  (1 / (1 + exp(sigma * ((((event_length - target_length) / target_length) * 2) - 1)))),
-                  1)
+    return(logistic_value)
+  }
+  
+  
+  get_CA_target = function(location_X, start_range = c(-42, 20), end_range = c(0, 42)) {
     
-    return(coef)
+    ### Compute the counter attack location_X target
+    ###
+    ### Inputs :
+    ###   - location_X (numeric), position of event on X axis
+    ###
+    ### Output :
+    ###   - target (numeric), position of target on X axis
+    
+    ratio = ifelse(location_X <= start_range[1], 1, (start_range[2] - location_X) / diff(start_range))
+    target = ifelse(ratio >= 0, end_range[2] - (diff(end_range) * ratio), NA)
+    
+    return(target)
+  }
+  
+  
+  get_CA_speed = function(possession, team_off, team_def) {
+    
+    ### Compute the counter attack speed
+    ###
+    ### Inputs :
+    ###  - possession (data.frame), events of one possession
+    ###  - team_off (list), name and index of offensive team
+    ###  - team_def (list), name and index of defensive team
+    ###  - speed_threshold (numeric vector),
+    ###       represent range of speed to compute counter-attack intensity
+    ###  - sigma (numeric), between .5 and 1
+    ###       parameter to tune the slope of sigmoid function
+    ###
+    ### Output :
+    ###  - intensity (numeric), represent counter-attack intensity of the possession
+    
+    team_name = as.character(possession$possession_team.name[1])
+    team_index = which(possession$team.name == team_name)
+    
+    x_start = possession$location_X[team_index[1]]
+    time_start = possession$time[team_index[1]]
+    x_target = get_CA_target(x_start)
+    
+    cond = possession$location_X[team_index] > x_target
+    if (length(which(cond)) == 0) {
+      # If the team doesn't reach x_target
+      speed = NA
+    } else {
+      # If the team reach x_target
+      x_prev = possession$location_X[head(team_index[which(cond)],1)-1]
+      time_prev = possession$time[head(team_index[which(cond)],1)-1]
+      x_next = possession$location_X[head(team_index[which(cond)],1)]
+      time_next = possession$time[head(team_index[which(cond)],1)]
+      
+      # Compute speed of movement between the start and the target
+      time_target = time_prev + ((time_next - time_prev) * (x_target - x_prev) / (x_next - x_prev))
+      speed = (x_target - x_start) / ((time_target - time_start)*60)  # speed m/s
+    }
+    
+    return(speed)
+    
+    ## Pour définir une contre attaque :
+    ## 1. On veut qu'une CA lente se rapproche vers 0, une CA rapide se rapproche vers 1
+    ## 2. CA définie par une zone de départ et une zone cible à atteindre : quelle zone cible ?
+    ##   2.1. Plus je suis proche de mon camp, plus la distance à parcourir doit être longue (relation linéaire)
+    ##        Plus je suis proche de mon camp, plus la zone cible est proche de moi
+    ##   2.2. Jusqu'à 20m dans le camp adverse, je peux prétendre à une contre-attaque
+    ##        La zone cible reste la même après une récupération dans mes 18m
+    ##   2.3. Au plus proche je dois atteindre le milieu du terrain
+    ##        Au plus loin je dois atteindre la surface adverse
+    ## 3. Je calcule la vitesse de déplacement du ballon entre la récupération et l'atteinte de la zone cible
+    ##    Cette vitesse me permettra de définir le degré d'importance de la contre-attaque
   }
 }
 
@@ -123,7 +192,9 @@ db_events = db_events %>%
   # Compute direct play index using :
   #   - pass and carry event
   #   - distance of the event
-  #   - angle between event direction and target (goal) direction 
+  #   - angle between event direction and target (goal) direction
+  #   - coefficient of event relevance for direct_play : if the event is forward direction
+  #     and length longer than target distance, the coefficient decrease according to logistic function
   mutate(
     target.angle = get_angle((60 - location_X), -location_Y),
     target.length = sqrt((60-location_X)^2 + location_Y^2),
@@ -132,23 +203,54 @@ db_events = db_events %>%
     event.length = sqrt(event.length_X^2 + event.length_Y^2),
     event.angle = get_angle(x=event.length_X, y=event.length_Y),
     event.adjusted_angle = get_adjusted_angle(target.angle, event.angle),
-    coef = get_directPlay_coef(event.length, target.length, event.adjusted_angle),
+    coef = ifelse((event.length > target.length) & cos(event.adjusted_angle) > 0 & event.length > 0,
+                  logistic_scale(value=(event.length - target.length) / target.length, sigma=-10), 1),
     index.direct_play = event.length * cos(event.adjusted_angle) * coef
-  ) %>%
-  select(!c(target.angle, target.length, event.length_X, event.length_Y, event.length, event.angle, event.adjusted_angle, coef))
+  )# %>%
+  #select(!c(target.angle, target.length, event.length_X, event.length_Y, event.length, event.angle, event.adjusted_angle, coef))
 
 
-# ggplot(db_events[which(db_events$type.name == "Pass" | db_events$type.name == "Carry"),]) +
-#   geom_segment(aes(x=0, y=0, xend=event.length_X, yend=event.length_Y, color=event.adjusted_angle), size=.05) +
-#   scale_color_gradient(low='purple', high='yellow') +
-#   scale_x_continuous(name="Left/right component") +
-#   scale_y_continuous(name="Forward/backward component") +
-#   labs(color = "Pass/carry adjusted angle")
-# 
-# ggplot(db_events[which(db_events$type.name == "Pass" | db_events$type.name == "Carry"),]) +
-#   geom_point(aes(x = event.adjusted_angle, y = event.length, color=index.direct_play)) +
-#   scale_color_gradient(low='purple', high='yellow') +
-#   scale_x_continuous(name="Pass/carry adjusted angle") +
-#   scale_y_continuous(name="Pass/carry length") +
-#   labs(color = "Direct play index")
+ggplot(db_events[which(is.na(db_events$index.direct_play) == FALSE),]) +
+  geom_point(aes(x=event.angle, y=event.length, color=event.adjusted_angle)) +
+  scale_color_gradient2(low="#ab3428", mid="#f5ee9e", high="#2d728f", midpoint=0) +
+  scale_x_reverse(name="Pass/carry angle", breaks=c(-pi, -pi/2, 0, pi/2, pi), labels=c("", "Right", "Forward", "Left", "Backward")) +
+  scale_y_continuous(name="Pass/carry length") +
+  labs(color = "Event adjusted angle") +
+  coord_polar(start=-pi)
+
+ggplot(db_events[which(is.na(db_events$index.direct_play) == FALSE),]) +
+  geom_point(aes(x=event.angle, y=event.length, color=index.direct_play)) +
+  scale_color_gradient2(low="#ab3428", mid="#f5ee9e", high="#2d728f", midpoint=0) +
+  scale_x_reverse(name="Pass/carry angle", breaks=c(-pi, -pi/2, 0, pi/2, pi), labels=c("", "Right", "Forward", "Left", "Backward")) +
+  scale_y_continuous(name="Pass/carry length") +
+  labs(color = "Direct play index") +
+  coord_polar(start=-pi)
+
+
+
+db_events$CA_speed = NA
+for (match.id in unique(db_events$match.id)) {
+  print(paste(match.id, length(unique(db_events$match.id)), sep=' / '))
+  teams = unique(as.character(db_events$possession_team.name[which(db_events$match.id == match.id)]))
+  team_off = list()
+  team_def = list()
+  for (possession in unique(db_events$possession[which(db_events$match.id == match.id)])) {
+    index = which(db_events$match.id == match.id & db_events$possession == possession)
+    temp_possession = db_events[index,]
+    
+    db_events$CA_speed[index] = get_CA_speed(temp_possession)
+  }
+}
+
+a = db_events %>%
+  group_by(match.id, possession) %>%
+  summarise(CA_speed = unique(CA_speed)) %>%
+  filter(is.na(CA_speed) == FALSE)
+
+ggplot(a, aes(x=CA_speed)) +
+  geom_density(aes(y=..density..)) +
+  scale_x_continuous(limits=c(-10, 40))
+
+
+
 
